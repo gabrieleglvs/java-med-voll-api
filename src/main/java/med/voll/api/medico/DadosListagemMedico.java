524abd09718083ee89d135d80813e24e18394704
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-13 15:39:08 -0300

    feat: atualizar dados do medico

--- src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,10 +1,11 @@
 package med.voll.api.medico;
 
-public record DadosListagemMedico(String nome,
+public record DadosListagemMedico(Long id,
+                                  String nome,
                                   String email,
                                   String crm,
                                   Especialidade especialidade) {
     public DadosListagemMedico(Medico d) {
-        this(d.getNome(), d.getEmail(), d.getCrm(), d.getEspecialidade());
+        this(d.getId(), d.getNome(), d.getEmail(), d.getCrm(), d.getEspecialidade());
     }
 }
