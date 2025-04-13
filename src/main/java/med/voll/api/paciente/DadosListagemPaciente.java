b19b5b577507513f2a15499a36e1dc1ab2d5f3d6
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-13 17:30:44 -0300

    feat: atualizar dados do Ãpaciente

--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -1,9 +1,10 @@
 package med.voll.api.paciente;
 
-public record DadosListagemPaciente(String nome,
+public record DadosListagemPaciente(Long id,
+                                    String nome,
                                     String email,
                                     String cpf) {
     public DadosListagemPaciente(Paciente dados) {
-        this(dados.getNome(), dados.getEmail(), dados.getCpf());
+        this(dados.getId(), dados.getNome(), dados.getEmail(), dados.getCpf());
     }
 }
