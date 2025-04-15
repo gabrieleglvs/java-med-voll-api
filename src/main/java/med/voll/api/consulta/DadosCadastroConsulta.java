e043ad3d62c3ce3642311d825c0917fc982f1c5d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-15 19:51:19 -0300

    refactor: simplificar dados do DTO DadosCadastroConsulta

--- src/main/java/med/voll/api/consulta/DadosCadastroConsulta.java
+++ src/main/java/med/voll/api/consulta/DadosCadastroConsulta.java
@@ -1,8 +1,5 @@
 package med.voll.api.consulta;
 
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.paciente.DadosCadastroPaciente;
-
 import java.time.LocalDateTime;
 
 public record DadosCadastroConsulta(Long paciente_id,
