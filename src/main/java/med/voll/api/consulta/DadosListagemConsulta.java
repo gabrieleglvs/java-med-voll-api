9fb044b4c227f22b32f14b3484a0adb7300a8d90
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-15 19:57:06 -0300

    feat: DTO para listar informações de consultas

--- src/main/java/med/voll/api/consulta/DadosListagemConsulta.java
+++ src/main/java/med/voll/api/consulta/DadosListagemConsulta.java
@@ -1,2 +1,11 @@
-package med.voll.api.consulta;public class DadosListagemConsulta {
+package med.voll.api.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosListagemConsulta(Long pacienteId,
+                                    Long medicoId,
+                                    LocalDateTime dataEHora) {
+    public DadosListagemConsulta(Consulta c) {
+        this(c.getPaciente().getId(), c.getMedico().getId(), c.getData_e_hora());
+    }
 }
