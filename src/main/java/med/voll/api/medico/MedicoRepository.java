999f10f93e05f428351092c5f58c5d08a98eaaa0
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-15 20:02:32 -0300

    feat: pegar nova lista de medicos sem paginacao

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    List<Medico> findAllByAtivoTrue();
 }
