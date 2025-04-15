a1623fdb1c402e3a06d9ca65dc02f2d3b5431735
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-15 20:05:15 -0300

    refactor: mudar logica de negocio de MedicoController para MedicoService

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,43 +1,43 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.*;
+import med.voll.api.service.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
     @Autowired
-    private MedicoRepository repository;
+    private MedicoService service;
 
     @PostMapping
-    @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+        service.cadastrar(dados);
     }
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao)
-                .map(DadosListagemMedico::new);
+        return service.listar(paginacao);
+    }
+
+    public List<DadosListagemMedico> listarMedicos(){
+        return service.listarMedicos();
     }
 
     @PutMapping
-    @Transactional
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
-        var medico = repository.getReferenceById(dados.id());
-        medico.atualizarInformacoes(dados);
+        service.atualizar(dados);
     }
 
     @DeleteMapping("/{id}")
-    @Transactional
     public void excluir(@PathVariable Long id) {
-        var medico = repository.getReferenceById(id);
-        medico.excluir();
+        service.excluir(id);
     }
 }
