799ac78269f987fab54a81ed774436a2798426c1
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-15 19:54:57 -0300

    feat: listar consultas para paciente e medico

--- src/main/java/med/voll/api/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/consulta/ConsultaRepository.java
@@ -1,6 +1,13 @@
 package med.voll.api.consulta;
 
+import med.voll.api.medico.Medico;
+import med.voll.api.paciente.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    List<Consulta> findByPaciente(Paciente paciente);
+
+    List<Consulta> findByMedico(Medico medico);
 }
