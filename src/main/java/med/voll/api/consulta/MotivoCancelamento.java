581c6d1a50c6e11997ed0b60849b83d568d9f16f
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-16 13:38:55 -0300

    feat: criar Enum para campo motivoCancelamento

--- src/main/java/med/voll/api/consulta/MotivoCancelamento.java
+++ src/main/java/med/voll/api/consulta/MotivoCancelamento.java
@@ -1,2 +1,18 @@
-package med.voll.api.consulta;public enum MotivoCancelamento {
+package med.voll.api.consulta;
+
+public enum MotivoCancelamento {
+    PACIENTE_DESISTIU("Paciente desistiu"),
+    MEDICO_CANCELADO("Médico cancelou"),
+    OUTROS("Outros"),
+    VAZIO("");
+
+    private final String descricao;
+
+    MotivoCancelamento(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
