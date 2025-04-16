cec12b1384b2914cde24ae4b2f9d363b5c3bbe63
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-16 13:41:01 -0300

    feat: listar consultas

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,9 +2,12 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import med.voll.api.consulta.DadosCadastroConsulta;
+import med.voll.api.consulta.DadosListagemConsulta;
 import med.voll.api.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consultas")
@@ -16,5 +19,10 @@ public class ConsultaController {
     @Transactional
     public void marcarConsulta(@RequestBody DadosCadastroConsulta dados) throws Exception {
         service.marcarConsulta(dados);
+    }
+
+    @GetMapping
+    public List<DadosListagemConsulta> listar() {
+        return service.listarConsulta();
     }
 }
