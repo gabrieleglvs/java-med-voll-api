e4aea033f0fb7a347a8825586a1995f5af7a141e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-17 20:41:38 -0300

    feat: criar metodo static para tratar descricao do Enum MotivoCancelamento

--- src/main/java/med/voll/api/consulta/MotivoCancelamento.java
+++ src/main/java/med/voll/api/consulta/MotivoCancelamento.java
@@ -1,8 +1,11 @@
 package med.voll.api.consulta;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MotivoCancelamento {
     PACIENTE_DESISTIU("Paciente desistiu"),
-    MEDICO_CANCELADO("Médico cancelou"),
+    MEDICO_CANCELOU("Médico cancelou"),
     OUTROS("Outros"),
     VAZIO("");
 
@@ -12,7 +15,18 @@ public enum MotivoCancelamento {
         this.descricao = descricao;
     }
 
+    @JsonValue
     public String getDescricao() {
         return descricao;
+    }
+
+    @JsonCreator
+    public static MotivoCancelamento fromDescricao(String descricao) {
+        for (MotivoCancelamento motivo : values()) {
+            if (motivo.getDescricao().equalsIgnoreCase(descricao)) {
+                return motivo;
+            }
+        }
+        throw new IllegalArgumentException("Motivo de cancelamento inválido: " + descricao);
     }
 }
