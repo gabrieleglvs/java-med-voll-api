80f82a154d97222e263a76cf416f5bbfc1c9d77e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-17 20:47:47 -0300

    feat: criar DTO para transportar dados do cancelamento

--- src/main/java/med/voll/api/consulta/DadosCancelarConsulta.java
+++ src/main/java/med/voll/api/consulta/DadosCancelarConsulta.java
@@ -1,2 +1,6 @@
-package med.voll.api.consulta;public record DadosCancelarConsulta() {
+package med.voll.api.consulta;
+
+
+public record DadosCancelarConsulta(Long id,
+                                    String motivo_cancelamento) {
 }
